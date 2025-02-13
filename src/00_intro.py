f3f960a2e67c1763242fbbf5362b448b26473110
suraphop <suraphop@mail.com>
2025-02-13 13:28:32 +0700

    up content basic python

---
+++ src/00_intro.py
@@ -0,0 +1,31 @@
+# %% [markdown]
+# test run jupyter
+
+# %%
+print("hello world2")
+
+# %%
+print("hello world")
+
+# %% [markdown]
+# # Hello world
+
+# %% [markdown]
+# ## Hello world
+
+# %% [markdown]
+# ### Hello world
+
+# %% [markdown]
+# #### Hello world
+
+# %% [markdown]
+# hello world
+
+# %% [markdown]
+# - bullet1
+# - butllet2
+#   - bullet3
+#     - bullet4
+
+
